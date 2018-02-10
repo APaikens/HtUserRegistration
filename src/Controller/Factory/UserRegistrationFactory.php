6bcaaf81e32078421c1a71f70fae684393372a2f
APaikens <shamanis@gmail.com>
2018-02-11 00:02:37 +0200

    New factory style

--- src/Controller/Factory/UserRegistrationFactory.php
+++ src/Controller/Factory/UserRegistrationFactory.php
@@ -7,10 +7,25 @@ use HtUserRegistration\Controller\UserRegistrationController;
 
 class UserRegistrationFactory implements FactoryInterface
 {
-    public function createService(ServiceLocatorInterface $controllers)
+    public function __invoke(\Interop\Container\ContainerInterface $container, $requestedName, array $options = null)
     {
-        $userRegistrationService = $controllers->getServiceLocator()->get('HtUserRegistration\UserRegistrationService');
+        $userRegistrationService = $container->get('HtUserRegistration\UserRegistrationService');
+        $passwordForm = $container->get('HtUserRegistration\SetPasswordForm');
+        $userMapper = $container->get('zfcuser_user_mapper');
+        $userRegistrationMapper = $container->get('HtUserRegistration\UserRegistrationMapper');
+        $moduleOptions = $container->get('HtUserRegistration\ModuleOptions');
 
-        return new UserRegistrationController($userRegistrationService);
+        return new UserRegistrationController($userRegistrationService, $passwordForm, $userMapper, $userRegistrationMapper, $moduleOptions);
+    }
+
+    /**
+     * Create service
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @return mixed
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        return $this->__invoke($serviceLocator,null);
     }
 }
