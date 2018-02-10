ec718ab9abead452eb37628c9d8a82118fdf2785
APaikens <shamanis@gmail.com>
2018-02-11 00:03:21 +0200

    New factory style

--- src/Factory/ModuleOptionsFactory.php
+++ src/Factory/ModuleOptionsFactory.php
@@ -7,8 +7,19 @@ use HtUserRegistration\Options\ModuleOptions;
 
 class ModuleOptionsFactory implements FactoryInterface
 {
+    public function __invoke(\Interop\Container\ContainerInterface $container, $requestedName, array $options = null)
+    {
+        return new ModuleOptions($container->get('config')['ht_user_registration']);
+    }
+
+    /**
+     * Create service
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @return mixed
+     */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        return new ModuleOptions($serviceLocator->get('Config')['ht_user_registration']);
+        return $this->__Invoke($serviceLocator,null);
     }
 }
